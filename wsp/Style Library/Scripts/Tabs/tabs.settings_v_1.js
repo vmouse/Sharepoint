903f52a12c40b1a4c14c089d97c4011dcd5660ec
Vlad <vlad.baginsky@mail.ru>
2015-10-02 18:21:55 +0300

    Beta release

--- wsp/Style Library/Scripts/Tabs/tabs.settings_v_1.js
+++ wsp/Style Library/Scripts/Tabs/tabs.settings_v_1.js
@@ -3,17 +3,19 @@
 var currentFormUniqueId,
 	currentFormWebPartId,
     baseViewID = '',
-	listId = 'E45D8F3C-EDB3-46EF-8731-D5A8FFA3A835', //Contacts list guid
+//	listId = 'E45D8F3C-EDB3-46EF-8731-D5A8FFA3A835', //Contacts list guid
     itemId = 0,
     zones = [
-    	{tabIndex:'2', zoneId:'MSOZoneCell_WebPartWPQ3'},
-    	{tabIndex:'3', zoneId:'MSOZoneCell_WebPartWPQ4'}
+    	{tabIndex:'2', zoneId:'MSOZoneCell_WebPartWPQ4'}, // Fund's contacts
+    	{tabIndex:'3', zoneId:'MSOZoneCell_WebPartWPQ5'}, // Fund's tasks
+    	{tabIndex:'4', zoneId:'MSOZoneCell_WebPartWPQ3'}  // Fund's dosuments
     ];
 
 var tabsObj = [ 
     ["Контактная информация", ["Code", "FundStatus", "ShortName", "WorkAddress", "WorkPhone", "_Comments", "Created", "EMail", "FullName", "ManagersName",  "Title", "WebPage"]], 
     ["Банковские реквизиты", ["Bank", "BIK", "HeadjobTitle", "HeadFL", "INN", "IFNS", "FSS_Code", "bank_corr_account", "KPP", "ORGN", "OKPO", "AuthorityBase", "bank_account", "RNS", "RN_FSS", "RN_FSS_Code", "SNILS", "HeadLN", "RegisterredAddr", "Authror", "Editor"]],
   	["Контакты", []],
+  	["Задачи", []],
   	["Документы", []]
 ];
 
@@ -28,6 +30,17 @@ SP.SOD.executeFunc("clienttemplates.js", "SPClientTemplates", function() {
     overrideCtx.OnPreRender = TabsOnPreRender;
     overrideCtx.OnPostRender = TabsOnPostRender;
     overrideCtx.Templates = {};
+  
+//    overrideCtx.Templates.Fields = {
+    //"FieldName": {
+      //  NewForm: <function_name>
+      //  EditForm: <function_name>
+      //},
+//    "Code": {
+//      NewForm: renderRequiredField,
+//      EditForm: renderRequiredField
+//      },
+//    };
 
     SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx); 
   }
