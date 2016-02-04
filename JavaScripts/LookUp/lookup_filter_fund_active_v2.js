fa4dfdd5a663f720fe553e595cc6ee26448a36dc
Vlad <vlad.baginsky@mail.ru>
2016-02-04 17:43:39 +0300

    first commit

---
+++ JavaScripts/LookUp/lookup_filter_fund_active_v2.js
@@ -0,0 +1,103 @@
+var FundsList = 'Фонды';
+var FundsView = 'Основные фонды';
+
+
+(function () {
+	var renderCtx = {};
+    renderCtx.Templates = {};
+    renderCtx.Templates.Fields = {
+        "Fund": { "NewForm": renderFund, "EditForm": renderFund},
+    };
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(renderCtx);
+})();
+
+
+function getListItems(webUrl, listTitle, queryText)
+{
+    var viewXml = '<View><Query>' + queryText + '</Query></View>';
+    var url = webUrl + "/_api/web/lists/getbytitle('" + listTitle + "')/getitems";
+    var queryPayload = {
+        'query' : {
+            '__metadata': { 'type': 'SP.CamlQuery' },
+            'ViewXml' : viewXml
+        }
+    };
+    return executeJson(url,"POST",null,queryPayload);
+}
+
+
+function getListViewItems(webUrl, listTitle, viewTitle)
+{
+    var url = webUrl + "/_api/web/lists/getByTitle('" + listTitle + "')/Views/getbytitle('" + viewTitle + "')/ViewQuery";
+    return executeJson(url).then(
+        function(data){
+            var viewQuery = data.d.ViewQuery;
+            return getListItems(webUrl,listTitle,viewQuery);
+        });
+}
+
+
+function executeJson(url, method, headers, payload, asyncCall)
+{
+    method = method || 'GET';
+    headers = headers || {};
+    asyncCall = asyncCall || false;
+    headers["Accept"] = "application/json;odata=verbose";
+    if(method == "POST") {
+        headers["X-RequestDigest"] = $("#__REQUESTDIGEST").val();
+    }
+    var ajaxOptions =
+    {
+        url: url,
+        type: method,
+        contentType: "application/json;odata=verbose",
+        async: asyncCall,
+        headers: headers
+    };
+    if (typeof payload != 'undefined') {
+        ajaxOptions.data = JSON.stringify(payload);
+    }
+    return $.ajax(ajaxOptions);
+}
+
+
+function renderFund(ctx){
+
+	var formCtx = SPClientTemplates.Utility.GetFormContextForCurrentField(ctx);
+    FundsSite = _spPageContextInfo.siteAbsoluteUrl;
+
+ 	 formCtx.registerGetValueCallback(formCtx.fieldName, function () {
+        if ($("[id$=" + formCtx.fieldName + "] option:selected").length != 0) {
+            var obj = $("[id$=" + formCtx.fieldName + "] option:selected");
+            var str = obj.val() + ';#' + obj.text();
+            return str;
+        }
+        else
+            return '';
+    }); 
+    var selectedValue = ctx.CurrentFieldValue;
+
+    var fieldHtml = '<select ';
+    fieldHtml += 'id="' + formCtx.fieldName + '">';
+
+    getListViewItems(FundsSite, FundsList, FundsView)
+        .done(function(data)
+        {
+            var items = data.d.results;
+            for(var i = 0; i < items.length;i++) {
+                console.log(items[i].Title);
+                if(selectedValue==items[i].ID+";#"+items[i].Title)
+                    fieldHtml += "<option selected value='" + items[i].ID + "'>" + items[i].Title + "</option>";
+                else
+                    fieldHtml += "<option value='" + items[i].ID + "'>" + items[i].Title + "</option>";
+            }
+        })
+        .fail(
+        function(error){
+            console.log(JSON.stringify(error));
+        })
+
+    fieldHtml += '</select>';
+    return fieldHtml;
+}
